f6b632f3e9ee3766a455b31a6850cda270989c61
karinEkenberg <137770952+karinEkenberg@users.noreply.github.com>
2024-11-14 12:20:48 +0100

    third commit

--- fr-advokatbyra-react/src/components/Header.jsx
+++ fr-advokatbyra-react/src/components/Header.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
   return (
-    <div className='text-white bg-primary'>Header</div>
-  )
+    <header className="bg-primary flex h-full">
+    <figure>
+        <img className="h-12" src="images/fr-favicon.svg" alt="favicon" />
+    </figure>
+      <nav>
+        <ul className="flex ">
+          <li>
+            <a href="#home">Hem</a>
+          </li>
+          <li>
+            <a href="#tjanster">Tjänster</a>
+          </li>
+          <li>
+            <a href="#medarbetare">Medarbetare</a>
+          </li>
+          <li>
+            <a href="#nyheter">Nyheter</a>
+          </li>
+          <li>
+            <a href="#about">Om oss</a>
+          </li>
+          <li>
+            <a href="#kontakt">Kontakta oss</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
