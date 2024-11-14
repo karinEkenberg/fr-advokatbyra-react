6287b2a1a7d2f12aaa99fcaf9313cd65cf968cd4
karinEkenberg <137770952+karinEkenberg@users.noreply.github.com>
2024-11-14 21:34:27 +0100

    favicon

--- fr-advokatbyra-react/src/components/Logo.jsx
+++ fr-advokatbyra-react/src/components/Logo.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 export const Logo = () => {
   return (
-    <figure >
-      <img className="h-12 w-auto pt-2" src="images/fr-logo.webp" alt="logo" />
+    <figure>
+      <img
+        className="h-12 w-auto pt-2"
+        src="images/fr-logo.webp"
+        alt="logo"
+      />
     </figure>
   );
 };
--- fr-advokatbyra-react/src/components/Tjanster.jsx
+++ fr-advokatbyra-react/src/components/Tjanster.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Tjanster = () => {
   return (
-    <ul className="absolute left-0 mt-2 w-40 bg-white text-black rounded shadow-lg">
-    <li className="px-4 py-2 hover:bg-gray-200">
+    <ul className="absolute left-0 mt-2 w-40 bg-primary bg-opacity-85 text-white rounded shadow-lg">
+    <li className="px-4 py-2 hover:bg-primary">
       <a href="#privat">Privatperson</a>
     </li>
-    <li className="px-4 py-2 hover:bg-gray-200">
+    <li className="px-4 py-2 hover:bg-primary">
       <a href="#hyresvard">Hyresvärd</a>
     </li>
-    <li className="px-4 py-2 hover:bg-gray-200">
+    <li className="px-4 py-2 hover:bg-primary">
       <a href="#foretag">Företag</a>
     </li>
   </ul>
