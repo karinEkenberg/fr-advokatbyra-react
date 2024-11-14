77f075e596b5c2e5de9064645b9655298dd581a5
karinEkenberg <137770952+karinEkenberg@users.noreply.github.com>
2024-11-14 12:11:56 +0100

    second commit

--- fr-advokatbyra-react/tailwind.config.js
+++ fr-advokatbyra-react/tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1C3457",
+        background: "#F0F0F0",
+        background2: "F5F5F5",
+        links: "#F28E2B",
+      },
+    },
   },
   plugins: [],
-}
+};
